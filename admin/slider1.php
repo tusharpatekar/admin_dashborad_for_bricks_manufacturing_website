7501fbcd8fe54418b14016575e832c9ecf4e443b
Tushar patekar <tusharpatekar45@gmail.com>
2023-04-08 23:11:57 +0530

    first commit

---
+++ admin/slider1.php
@@ -0,0 +1,23 @@
+<?php include('includes/header.php');?>
+
+
+
+
+<div class="container" left:300px>
+    <div class="row">
+    <h1>Chose image option</h1>
+<div class="select">
+   <select name="format" id="format">
+      <option selected disabled>Choose Image option</option>
+      <option value="image1">image1</option>
+      <option value="image2">image2</option>
+      <option value="image3">image3</option>
+
+   </select>
+</div>
+
+        
+    </div>
+    
+</div>
+<?php include('includes/footer.php'); ?>
